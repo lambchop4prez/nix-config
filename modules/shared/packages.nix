7e5937adb51c8f6d4d48b9ff4cb1d54c740e1b7c
Tim <tmc@Lenny.lan>
2025-01-26 11:57:16 -0500

    feat: Add more packages

--- modules/shared/packages.nix
+++ modules/shared/packages.nix
@@ -1,8 +1,32 @@
 { pkgs, ... }: 
 with pkgs; [
-    git
-    vim
+    age
+    coreutils
     curl
+    direnv
+    docker
+    docker-buildx
+    docker-compose
     gh
-    
+    git
+    gnumake
+    go
+    go-task
+    grype
+
+    hackrf
+    httpie
+    ipcalc
+    iperf
+    ipmitool
+    kubectl
+    kubernetes-helm
+    nmap
+    nodejs
+    sops
+    syft
+    tree
+    vim
+    wget
+    zsh
 ]
