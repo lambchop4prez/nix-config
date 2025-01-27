c454ec0cb96845f77a079b1f72459d9a1f402214
Tim <tmc@Lenny.lan>
2025-01-27 10:57:35 -0500

    chore: Adding more packages

--- modules/shared/packages.nix
+++ modules/shared/packages.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }: 
 with pkgs; [
     age
+    age-plugin-yubikey
+    bat
     coreutils
     curl
     direnv
@@ -10,23 +12,31 @@ with pkgs; [
     gh
     git
     gnumake
+    gnupg
     go
     go-task
     grype
-
     hackrf
     httpie
     ipcalc
     iperf
     ipmitool
+    jq
+    jwt-cli
     kubectl
     kubernetes-helm
     nmap
     nodejs
+    nodePackages.prettier
+    nodePackages.npm
+    openssh
+    opentofu
     sops
     syft
     tree
     vim
     wget
+    yq
+    zip
     zsh
 ]
